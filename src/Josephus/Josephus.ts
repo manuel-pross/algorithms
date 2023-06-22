c477df0c447ead625721198987823e8efc3f9220
fhs49068 <mpross.mmt-m2022@fh-salzburg.ac.at>
2023-06-22 18:03:25 +0200

    feat: add josephus

---
+++ src/Josephus/Josephus.ts
@@ -0,0 +1,12 @@
+export function recJosephus(n: number): number {
+  if (n === 0 || n === 1) return 1;
+
+  if (n % 2 === 0) return recJosephus(n / 2) * 2 - 1;
+  else return recJosephus(Math.floor(n / 2)) * 2 + 1;
+}
+
+export function closedJosephus(n: number): number {
+  const m = Math.floor(Math.log(n) / Math.log(2));
+  const l = n - Math.pow(2, m);
+  return 2 * l + 1;
+}
--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,23 @@
 import { fibLinear, fibMatrix, fibRec } from "./Fibonacci/fibonacci";
+import { closedJosephus, recJosephus } from "./Josephus/Josephus";
 
-console.log(fibRec(0));
-console.log(fibRec(1));
-console.log(fibRec(2));
-console.log(fibRec(3));
-console.log(fibRec(4));
-console.log(fibRec(5));
-console.log(fibRec(20));
+// console.log(fibRec(0));
+// console.log(fibRec(1));
+// console.log(fibRec(2));
+// console.log(fibRec(3));
+// console.log(fibRec(4));
+// console.log(fibRec(5));
+// console.log(fibRec(20));
 
-console.log(fibLinear(0));
-console.log(fibLinear(1));
-console.log(fibLinear(2));
-console.log(fibLinear(3));
-console.log(fibLinear(4));
-console.log(fibLinear(5));
-console.log(fibLinear(20));
+// console.log(fibLinear(0));
+// console.log(fibLinear(1));
+// console.log(fibLinear(2));
+// console.log(fibLinear(3));
+// console.log(fibLinear(4));
+// console.log(fibLinear(5));
+// console.log(fibLinear(20));
 
-console.log(fibMatrix(20));
+// console.log(fibMatrix(20));
+
+console.log(recJosephus(10));
+console.log(closedJosephus(10));
